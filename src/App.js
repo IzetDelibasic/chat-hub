5773722becaf58a79697294e4f2ebc0e1e14a2de
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-05 00:42:12 +0200

    Adding authentication and room input functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
-import React from 'react';
+import React, { useState , useRef} from 'react';
 import { Auth } from './components/Auth';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
 function App() {
-  return (
-    <div className="App">
-      <Auth />
-    </div>
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  if(!isAuth){
+    return (
+      <div className='App'> 
+        <Auth setIsAuth={setIsAuth} />
+      </div>
+    );
+  }
+
+  return ( 
+    <div>{room ? (<div> Chat </div>
+  ) : (
+    <div className='room'> 
+      <label>Enter Room Name:</label>
+      <input ref={roomInputRef}/>
+      <button onClick={() => setRoom(roomInputRef.current.value)}> Enter Chat</button>
+    </div>  
+
+  )}
+  </div>
   );
 }
 
