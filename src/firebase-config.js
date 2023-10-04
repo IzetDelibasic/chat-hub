eff4b276645cb368d2bcf377526617dc166ce832
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-04 19:29:33 +0200

    Setting up firebase and authentication

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA91aSf7z9EQG37Vie8970r9iUnIZRQYdA",
+  authDomain: "chathub-43307.firebaseapp.com",
+  projectId: "chathub-43307",
+  storageBucket: "chathub-43307.appspot.com",
+  messagingSenderId: "604181567987",
+  appId: "1:604181567987:web:6343137b8e5722fba49a86"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
