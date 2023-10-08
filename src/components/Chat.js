ac3a4152d6f95bef41b9ac49f36ae6d6c96d7e82
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-08 20:01:24 +0200

    Real-time firestore database updating

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,15 +1,28 @@
-import { useState } from "react";
-import {addDoc, collection, serverTimestamp} from 'firebase/firestore';
+import { useEffect, useState } from "react";
+import {addDoc, collection, onSnapshot, serverTimestamp, query, where} from 'firebase/firestore';
 import { auth, db } from "../firebase-config"
 import '../styles/Chat.css';
 
 export const Chat = (props) => {
 
     const {room} = props; 
-
     const [ newMessage, setNewMessage] = useState("");
+    const [messages, setMessages] = useState([]);
 
     const messagesRef = collection(db, "messages");
+
+    useEffect(() =>{
+        const queryMessages = query(messagesRef, where("room", "==", room));
+        const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+            let messages = [];
+            snapshot.forEach((doc) => {
+                messages.push({...doc.data(), id : doc.id});
+            });
+            setMessages(messages);
+        });
+        return() => unsuscribe();
+    }, []);
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -25,17 +38,20 @@ export const Chat = (props) => {
         setNewMessage("");
     };
 
-    return <div className="chat-app"> 
-        <form onSubmit={handleSubmit} className="new-message-form">
-            <input 
-                className="new-message-input"
-                placeholder="Type your message here"
-                onChange={(e) => setNewMessage(e.target.value)}
-                value={newMessage}
-            />
-            <button type="submit" className="send-button">
-                Send message
-            </button>
-        </form>
-    </div>
+    return(
+        <div className="chat-app">
+            <div>{messages.map((message) => <h1>{message.text}</h1>)}</div> 
+            <form onSubmit={handleSubmit} className="new-message-form">
+                <input 
+                    className="new-message-input"
+                    placeholder="Type your message here"
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                />
+                <button type="submit" className="send-button">
+                    Send message
+                </button>
+            </form>
+        </div>
+    ) 
 }
